c63ff1ab86696c1fa65d5eacec3be40b17b4c54d
vithika12345 <vithikasaoji@gmail.com>
2026-04-24 14:38:25 +0530

    DoubleCollection.java

---
+++ DoubleCollection.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class DoubleCollection {
+
+	public static void main(String[] args) {
+		ArrayList<Double> arrList = new ArrayList<Double>();
+		System.out.println(arrList);
+		arrList.add(10.0909);
+		System.out.println(arrList);
+		arrList.add(11.0909);
+		arrList.add(12.0909);
+		arrList.add(1.0909);
+		System.out.println(arrList);
+		
+		int i = 10;
+		Integer obj = i;
+
+	}
+
+}
