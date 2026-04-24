44c004ab449fd158e91ed58d93e97630bde30a61
vithika12345 <vithikasaoji@gmail.com>
2026-04-24 14:40:58 +0530

    InetegerCollection.java

---
+++ InetegerCollection.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class InetegerCollection {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> arrList = new ArrayList<Integer>();
+		System.out.println(arrList);
+		arrList.add(10);
+		System.out.println(arrList);
+		arrList.add(11);
+		arrList.add(12);
+		arrList.add(13);
+		arrList.add(14);
+		arrList.add(15);
+		System.out.println(arrList);
+
+	}
+
+}
