9e0eca7c7ce5c6fff2ee521eadd3382ce8f6436d
vithika12345 <vithikasaoji@gmail.com>
2026-04-24 14:37:11 +0530

    ArithmeticOperations.java

---
+++ ArithmeticOperations.java
@@ -0,0 +1,49 @@
+public class ArithmeticOperations {
+	private int a;
+	private int b;
+	private int c;
+	
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	public int getC() {
+		return c;
+	}
+
+	public void setC(int c) {
+		this.c = c;
+	}
+
+	public void addition()
+	{
+		
+		c = a + b;
+		//System.out.println("Addition is: " + c);
+	}
+	public void subtraction()
+	{
+		
+		c = a - b;
+		//System.out.println("Addition is: " + c);
+	}
+	public void multiplication()
+	{
+		
+		c = a * b;
+		//System.out.println("Addition is: " + c);
+	}
+	
+}
